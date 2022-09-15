46d4fefdc36c13bcce313b8631824f289b4753d9
teddy <teddyjh@protonmail.com>
2022-09-15 14:59:38 +0200

    Add 2 cells to table view and add simple config to the cells

--- Challange05/CountryNamesViewController.swift
+++ Challange05/CountryNamesViewController.swift
@@ -14,6 +14,17 @@ class CountryNamesViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country")!
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Country #\(indexPath.row + 1)"
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
 }
 
