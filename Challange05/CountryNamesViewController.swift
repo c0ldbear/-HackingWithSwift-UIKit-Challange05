3da08bcfb9e0c4825768729cbbbebca9c614a12e
teddy <teddyjh@protonmail.com>
2022-09-15 14:54:15 +0200

    Rename view controller: CountryNamesViewController
    With the rename of the file, it is also changed to inherit from
    the UITableViewController to be a table view instead

--- Challange05/CountryNamesViewController.swift
+++ Challange05/CountryNamesViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CountryNamesViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
